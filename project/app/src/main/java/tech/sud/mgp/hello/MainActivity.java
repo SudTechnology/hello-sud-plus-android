a83b5dce94639c6154e43471db28973e1b663cee
liuxueping <liuxueping@divtoss.com>
2022-01-19 16:23:50 +0800

    feat:增加启动页登陆页面以及一些资源文件

--- project/app/src/main/java/tech/sud/mgp/hello/MainActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/MainActivity.java
@@ -1,14 +1,35 @@
 package tech.sud.mgp.hello;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import tech.sud.mgp.hello.login.LoginActivity;
+import tech.sud.mgp.hello.utils.AppSharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler.postDelayed(this::jumpPage,2000);
+    }
+
+    private void jumpPage(){
+        String userId = AppSharedPreferences.getSP().getString(AppSharedPreferences.USER_ID_KEY,"");
+        if (userId.isEmpty()){
+            startActivity(new Intent(this, LoginActivity.class));
+        }else {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+        handler = null;
     }
 }
---
+++ project/app/src/main/java/tech/sud/mgp/hello/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package tech.sud.mgp.hello.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tech.sud.mgp.hello.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

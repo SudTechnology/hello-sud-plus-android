bb01762e619b462e470a934e677479572c44cad7
liuxueping <liuxueping@divtoss.com>
2022-01-24 17:24:15 +0800

    fat：增加首页场景游戏自定义view以及适配器等

--- project/app/src/main/java/tech/sud/mgp/hello/home/model/SceneModel.java
+++ project/app/src/main/java/tech/sud/mgp/hello/home/model/SceneModel.java
@@ -3,7 +3,31 @@ package tech.sud.mgp.hello.home.model;
 import java.util.List;
 
 public class SceneModel {
-    int sceneImgResId;
-    List<GameModel> gameModels;
-    int sceneTextResId;
+    int sceneId;
+    String sceneName;
+    String sceneImage;
+
+    public int getSceneId() {
+        return sceneId;
+    }
+
+    public void setSceneId(int sceneId) {
+        this.sceneId = sceneId;
+    }
+
+    public String getSceneName() {
+        return sceneName;
+    }
+
+    public void setSceneName(String sceneName) {
+        this.sceneName = sceneName;
+    }
+
+    public String getSceneImage() {
+        return sceneImage;
+    }
+
+    public void setSceneImage(String sceneImage) {
+        this.sceneImage = sceneImage;
+    }
 }

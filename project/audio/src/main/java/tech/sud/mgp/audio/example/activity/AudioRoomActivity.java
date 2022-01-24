793d734383167912d2b1dbb1bf95ebe2971a828c
liwensi <liwensi@divtoss.com>
2022-01-24 19:51:12 +0800

    feat：搭建语聊房页面及其顶部View

---
+++ project/audio/src/main/java/tech/sud/mgp/audio/example/activity/AudioRoomActivity.java
@@ -0,0 +1,13 @@
+package tech.sud.mgp.audio.example.activity;
+
+import tech.sud.mgp.audio.R;
+import tech.sud.mgp.common.base.BaseActivity;
+
+public class AudioRoomActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.audio_activity_room;
+    }
+
+}

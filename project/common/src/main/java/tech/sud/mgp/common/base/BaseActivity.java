262907a4a24f5dd3ff4df4fe2f5d840e3070c155
liwensi <liwensi@divtoss.com>
2022-01-25 12:26:38 +0800

    fix：修改HSUserInfo的使用方式

--- project/common/src/main/java/tech/sud/mgp/common/base/BaseActivity.java
+++ project/common/src/main/java/tech/sud/mgp/common/base/BaseActivity.java
@@ -37,17 +37,17 @@ public abstract class BaseActivity extends RxAppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putLong("userId", HSUserInfo.getInstance().userId);
-        outState.putString("nickName", HSUserInfo.getInstance().nickName);
-        outState.putString("gender", HSUserInfo.getInstance().gender);
+        outState.putLong("userId", HSUserInfo.userId);
+        outState.putString("nickName", HSUserInfo.nickName);
+        outState.putString("gender", HSUserInfo.gender);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        HSUserInfo.getInstance().userId = savedInstanceState.getLong("userId");
-        HSUserInfo.getInstance().nickName = savedInstanceState.getString("nickName");
-        HSUserInfo.getInstance().gender = savedInstanceState.getString("gender");
+        HSUserInfo.userId = savedInstanceState.getLong("userId");
+        HSUserInfo.nickName = savedInstanceState.getString("nickName");
+        HSUserInfo.gender = savedInstanceState.getString("gender");
     }
 
 }
--- project/common/src/main/java/tech/sud/mgp/common/model/HsUserInfo.java
+++ project/common/src/main/java/tech/sud/mgp/common/model/HsUserInfo.java
@@ -1,17 +1,7 @@
 package tech.sud.mgp.common.model;
 
 public class HSUserInfo {
-
-    private static final HSUserInfo sInstance = new HSUserInfo();
-
-    private HSUserInfo() {
-    }
-
-    public static HSUserInfo getInstance() {
-        return sInstance;
-    }
-
-    public long userId;
-    public String nickName;
-    public String gender; //male为男性，female为女性
+    public static long userId;
+    public static String nickName;
+    public static String gender; //male为男性，female为女性
 }

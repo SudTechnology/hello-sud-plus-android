452da946c0c4b73afad26bc6be68de9c672230c7
liwensi <liwensi@divtoss.com>
2022-01-25 17:40:33 +0800

    feat：增加房间公屏的View

---
+++ project/audio/src/main/java/tech/sud/mgp/audio/example/model/RoomTextModel.java
@@ -0,0 +1,7 @@
+package tech.sud.mgp.audio.example.model;
+
+public class RoomTextModel {
+    public String avatar;
+    public String nickName;
+    public String text;
+}
---
+++ project/audio/src/main/java/tech/sud/mgp/audio/example/widget/view/chat/RoomTextProvider.java
@@ -0,0 +1,33 @@
+package tech.sud.mgp.audio.example.widget.view.chat;
+
+import androidx.annotation.NonNull;
+
+import com.chad.library.adapter.base.provider.BaseItemProvider;
+import com.chad.library.adapter.base.viewholder.BaseViewHolder;
+
+import tech.sud.mgp.audio.R;
+import tech.sud.mgp.audio.example.model.RoomTextModel;
+import tech.sud.mgp.common.utils.ImageLoader;
+import tech.sud.mgp.common.widget.view.round.RoundedImageView;
+
+public class RoomTextProvider extends BaseItemProvider<Object> {
+    @Override
+    public int getItemViewType() {
+        return RoomChatAdapter.TYPE_TEXT;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.audio_item_room_text;
+    }
+
+    @Override
+    public void convert(@NonNull BaseViewHolder baseViewHolder, Object o) {
+        RoomTextModel item = (RoomTextModel) o;
+        RoundedImageView ivIcon = baseViewHolder.getView(R.id.riv_avatar);
+        ImageLoader.loadImage(ivIcon, item.avatar);
+        String nickName = item.nickName.isEmpty() ? "" : item.nickName;
+        baseViewHolder.setText(R.id.tv_name, nickName + "：");
+        baseViewHolder.setText(R.id.tv_text, item.text);
+    }
+}

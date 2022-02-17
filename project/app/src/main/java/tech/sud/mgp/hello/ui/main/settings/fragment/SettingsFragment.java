2c320487eccee9a12cb141161f8d39f56d949e95
liwensi <liwensi@divtoss.com>
2022-02-17 21:50:30 +0800

    feat：设置页展示大小的赋值

--- project/app/src/main/java/tech/sud/mgp/hello/ui/main/settings/fragment/SettingsFragment.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/main/settings/fragment/SettingsFragment.java
@@ -3,6 +3,7 @@ package tech.sud.mgp.hello.ui.main.settings.fragment;
 import android.content.Intent;
 import android.graphics.Color;
 import android.view.View;
+import android.widget.TextView;
 
 import com.blankj.utilcode.util.ToastUtils;
 
@@ -16,6 +17,7 @@ import tech.sud.mgp.hello.common.utils.HsIntentUtils;
 import tech.sud.mgp.hello.ui.main.settings.activity.ChangeRtcActivity;
 import tech.sud.mgp.hello.ui.main.settings.activity.UserAgreementActivity;
 import tech.sud.mgp.hello.ui.main.settings.activity.VersionInfoActivity;
+import tech.sud.mgp.hello.ui.room.common.gift.utils.FileUtils;
 
 /**
  * 首页当中的设置页
@@ -23,6 +25,7 @@ import tech.sud.mgp.hello.ui.main.settings.activity.VersionInfoActivity;
 public class SettingsFragment extends BaseFragment implements View.OnClickListener {
 
     private AppSizeView appSizeView;
+    private TextView tvTotalSize;
     private SettingButton btnVersionInfo;
     private SettingButton btnChangeRtc;
     private SettingButton btnChangeLanguage;
@@ -55,6 +58,7 @@ public class SettingsFragment extends BaseFragment implements View.OnClickListen
         btnOpenSource = findViewById(R.id.button_open_source_licenses);
         btnUserAgreement = findViewById(R.id.button_user_agreement);
         btnPrivacyPolicy = findViewById(R.id.button_privacy_policy);
+        tvTotalSize = findViewById(R.id.tv_total_size);
     }
 
     @Override
@@ -65,12 +69,14 @@ public class SettingsFragment extends BaseFragment implements View.OnClickListen
 
     private void initAppSize() {
         List<AppSizeView.AppSizeModel> list = new ArrayList<>();
-        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#fc955b"), "SudMGP Core", 113240));
-        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#fc5bca"), "SudMGP ASR", 1761234));
-        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#614bff"), "HelloSud", 1777234));
-        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#33000000"), "Zego RTC SDK", 152345));
-        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#1a000000"), "Agora RTC SDK", 1234500));
+        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#fc955b"), "SudMGP Core", 590848));
+        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#fc5bca"), "SudMGP ASR", 76800));
+        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#614bff"), "HelloSud", 20094157));
+        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#33000000"), "Zego RTC SDK", 21307064));
+        list.add(new AppSizeView.AppSizeModel(Color.parseColor("#1a000000"), "Agora RTC SDK", 24431820));
         appSizeView.setDatas(list);
+
+        tvTotalSize.setText(getString(R.string.total_value, FileUtils.formatFileSize(66500689)));
     }
 
     @Override

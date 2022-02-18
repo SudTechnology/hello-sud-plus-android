0b219b2e57a8957a63404c59533990bf4cb9a396
liwensi <liwensi@divtoss.com>
2022-02-18 10:55:11 +0800

    增加空的语音引擎实现类

---
+++ project/app/src/main/java/tech/sud/mgp/hello/rtc/audio/impl/aliyun/AliyunAudioEngineImpl.java
@@ -0,0 +1,76 @@
+package tech.sud.mgp.hello.rtc.audio.impl.aliyun;
+
+import tech.sud.mgp.hello.rtc.audio.core.AudioRoomConfig;
+import tech.sud.mgp.hello.rtc.audio.core.AudioUser;
+import tech.sud.mgp.hello.rtc.audio.core.IAudioEngine;
+import tech.sud.mgp.hello.rtc.audio.core.IAudioEventHandler;
+
+/**
+ * 阿里云引擎实现类
+ */
+public class AliyunAudioEngineImpl implements IAudioEngine {
+    @Override
+    public void setEventHandler(IAudioEventHandler handler) {
+
+    }
+
+    @Override
+    public void config(String appId, String appKey) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void loginRoom(String roomId, AudioUser user, AudioRoomConfig config) {
+
+    }
+
+    @Override
+    public void logoutRoom() {
+
+    }
+
+    @Override
+    public void startPublish(String streamId) {
+
+    }
+
+    @Override
+    public void stopPublishStream() {
+
+    }
+
+    @Override
+    public void startPlayingStream(String streamId) {
+
+    }
+
+    @Override
+    public void stopPlayingStream(String streamId) {
+
+    }
+
+    @Override
+    public void sendCommand(String roomId, String command, SendCommandResult result) {
+
+    }
+
+    @Override
+    public void setAudioDataHandler() {
+
+    }
+
+    @Override
+    public void startAudioDataListener() {
+
+    }
+
+    @Override
+    public void stopAudioDataListener() {
+
+    }
+}

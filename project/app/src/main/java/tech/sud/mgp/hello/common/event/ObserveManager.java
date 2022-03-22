1025beee06c99ad22c001946d96005bb70c89adb
liuxueping <liuxueping@divtoss.com>
2022-03-22 17:11:06 +0800

    feat:增加ob管理remove方法

--- project/app/src/main/java/tech/sud/mgp/hello/common/event/ObserveManager.java
+++ project/app/src/main/java/tech/sud/mgp/hello/common/event/ObserveManager.java
@@ -12,17 +12,42 @@ public class ObserveManager {
     /**
      * 切换语言后的事件通知
      */
-    public static void obChangeLanguage(ObserveListener listener) {
-        Observer observer = o -> {
+    private Observer<ChangeLanguageEvent> mChangeLanguageObserver;
+
+    private static ObserveManager observeManager;
+
+    private ObserveManager() {
+    }
+
+    public static ObserveManager getInstance() {
+        if (observeManager == null) {
+            observeManager = new ObserveManager();
+        }
+        return observeManager;
+    }
+
+    /**
+     * 切换语言后的事件通知观察
+     */
+    public void observeForever(ObserveListener listener) {
+        mChangeLanguageObserver = o -> {
             if (listener != null) {
                 listener.onChanged(o);
             }
         };
-        LiveEventBus.get(LiveEventBusKey.KEY_CHANGE_LANGUAGE).observeForever(observer);
+        LiveEventBus.get(LiveEventBusKey.KEY_CHANGE_LANGUAGE,ChangeLanguageEvent.class).observeForever(mChangeLanguageObserver);
+    }
+
+    /**
+     * 切换语言后的事件通知移除
+     */
+    public void removeObserver() {
+        if (mChangeLanguageObserver != null) {
+            LiveEventBus.get(LiveEventBusKey.KEY_CHANGE_LANGUAGE,ChangeLanguageEvent.class).removeObserver(mChangeLanguageObserver);
+        }
     }
 
     public interface ObserveListener {
         void onChanged(Object o);
     }
-
 }

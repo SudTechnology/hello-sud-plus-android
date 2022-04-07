5585e48c29016cdb01190e654b5c109c1e148e1b
liwensi <liwensi@divtoss.com>
2022-04-07 16:45:09 +0800

    feat：修改网络错误时的提示语

--- project/app/src/main/java/tech/sud/mgp/hello/common/http/rx/RxCallback.java
+++ project/app/src/main/java/tech/sud/mgp/hello/common/http/rx/RxCallback.java
@@ -3,18 +3,12 @@ package tech.sud.mgp.hello.common.http.rx;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ThreadUtils;
 import com.blankj.utilcode.util.ToastUtils;
-
-import org.json.JSONException;
-
-import java.net.ConnectException;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
+import com.blankj.utilcode.util.Utils;
 
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
-import retrofit2.HttpException;
+import tech.sud.mgp.hello.R;
 import tech.sud.mgp.hello.common.http.param.BaseResponse;
-import tech.sud.mgp.hello.common.http.param.ErrorStatus;
 import tech.sud.mgp.hello.common.http.param.RetCode;
 
 /**
@@ -40,25 +34,21 @@ public class RxCallback<T> implements Observer<BaseResponse<T>> {
     public void onError(Throwable e) {
         LogUtils.d("onError", e);
         ThreadUtils.runOnUiThread(() -> {
-            if (e instanceof SocketTimeoutException) { // 连接超时
-                onError(e, ErrorStatus.HTTP_TIMEOUT);
-            } else if (e instanceof ConnectException || e instanceof UnknownHostException) { // 无网络
-                onError(e, ErrorStatus.HTTP_UNCONNECTED);
-            } else if (e instanceof HttpException) { // 网络异常
-                onError(e, ErrorStatus.HTTP_EXCEPTION);
-            } else if (e instanceof JSONException) { // 数据异常
-                onError(e, ErrorStatus.DATA_EXCEPTION);
-            } else {// 其他错误
-                onError(e, ErrorStatus.OTHER_EXCEPTION);
-            }
+            processError(e);
         });
     }
 
-    private void onError(Throwable e, final ErrorStatus otherException) {
+    private void processError(Throwable e) {
         if (e != null) {
             e.printStackTrace();
         }
-        ToastUtils.showShort(otherException.getErrorMessage());
+//        if (e instanceof SocketTimeoutException) { // 连接超时
+//        } else if (e instanceof ConnectException || e instanceof UnknownHostException) { // 无网络
+//        } else if (e instanceof HttpException) { // 网络异常
+//        } else if (e instanceof JSONException) { // 数据异常
+//        } else {// 其他错误
+//        }
+        ToastUtils.showShort(Utils.getApp().getString(R.string.net_error_hint));
     }
 
     @Override

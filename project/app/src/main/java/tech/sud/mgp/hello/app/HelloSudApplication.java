e74448fc5d79de9b3c7924c554713310b7e66fa0
liwensi <liwensi@divtoss.com>
2022-04-18 16:04:37 +0800

    feat：去除网易云信Sdk，计算包增量补充提交

--- project/app/src/main/java/tech/sud/mgp/hello/app/HelloSudApplication.java
+++ project/app/src/main/java/tech/sud/mgp/hello/app/HelloSudApplication.java
@@ -12,8 +12,6 @@ import android.view.Gravity;
 import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ToastUtils;
-import com.netease.nimlib.sdk.NIMClient;
-import com.netease.nimlib.sdk.SDKOptions;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import java.io.BufferedReader;
@@ -44,10 +42,6 @@ public class HelloSudApplication extends Application {
     }
 
     private void initMinClient() {
-        SDKOptions options = new SDKOptions();
-        options.reducedIM = true;
-        options.disableAwake = true;
-        NIMClient.config(getApplicationContext(), null, options);
     }
 
     // Bugly config

cbe59c448e6439355ec1b90f189880c8f9d90ed2
liwensi <liwensi@sud.tech>
2022-05-27 19:23:39 +0800

    fix：修复多语言相关的问题

--- project/QuickStart/src/main/java/tech/sud/mgp/hello/common/widget/view/MarqueeTextView.java
+++ project/QuickStart/src/main/java/tech/sud/mgp/hello/common/widget/view/MarqueeTextView.java
@@ -2,6 +2,7 @@ package tech.sud.mgp.hello.common.widget.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Rect;
 import android.os.Build;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -57,4 +58,23 @@ public class MarqueeTextView extends androidx.appcompat.widget.AppCompatTextView
         setText(getText());
     }
 
+    /**
+     * 用于EditText抢注焦点的问题
+     */
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        if (focused) {
+            super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        }
+    }
+
+    /**
+     * Window与Window间焦点发生改变时的回调
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        if (hasWindowFocus) {
+            super.onWindowFocusChanged(hasWindowFocus);
+        }
+    }
 }

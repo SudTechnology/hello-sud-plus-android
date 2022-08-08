7661c3089c257a2f19c3efe8f320071c182a2675
liwensi <liwensi@sud.tech>
2022-08-08 20:04:08 +0800

    feat：解决custom场景逃跑不生效的问题

--- project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/custom/viewmodel/CustomGameViewModel.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/custom/viewmodel/CustomGameViewModel.java
@@ -4,33 +4,33 @@ import tech.sud.mgp.hello.ui.scenes.base.viewmodel.AppGameViewModel;
 
 public class CustomGameViewModel extends AppGameViewModel {
 
-    //加入游戏
-    public void joinGame() {
+    // 加入游戏
+    public void actionJoinGame() {
         sudFSTAPPDecorator.notifyAPPCommonSelfIn(true, -1, true, 1);
     }
 
-    //退出游戏
-    public void exitGame() {
+    // 退出游戏
+    public void actionExitGame() {
         sudFSTAPPDecorator.notifyAPPCommonSelfIn(false, -1, true, 1);
     }
 
-    //准备
-    public void readyGame() {
+    // 准备
+    public void actionReadyGame() {
         sudFSTAPPDecorator.notifyAPPCommonSelfReady(true);
     }
 
-    //取消准备
-    public void cancelReadyGame() {
+    // 取消准备
+    public void actionCancelReadyGame() {
         sudFSTAPPDecorator.notifyAPPCommonSelfReady(false);
     }
 
-    //开始游戏
-    public void startGame() {
+    // 开始游戏
+    public void actionStartGame() {
         sudFSTAPPDecorator.notifyAPPCommonSelfPlaying(true, "");
     }
 
-    //逃跑
-    public void escapeGame() {
+    // 逃跑
+    public void actionEscapeGame() {
         sudFSTAPPDecorator.notifyAPPCommonSelfPlaying(false, "");
     }
 

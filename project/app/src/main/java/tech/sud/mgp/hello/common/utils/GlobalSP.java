e79d7afbc45c3cfbf03ed2e4d1dbfcfdcec0e886
liwensi <liwensi@sud.tech>
2022-09-23 12:36:58 +0800

    feat：处理NFT旧版本绑定问题

--- project/app/src/main/java/tech/sud/mgp/hello/common/utils/GlobalSP.java
+++ project/app/src/main/java/tech/sud/mgp/hello/common/utils/GlobalSP.java
@@ -14,17 +14,15 @@ public class GlobalSP {
     public final static String USER_TOKEN_KEY = "user_token_key"; // 登录用户Token
     public final static String USER_REFRESHTOKEN_KEY = "user_refreshtoken_key"; // 登录刷新Token
     public final static String USER_HEAD_PORTRAIT_KEY = "user_head_portrait_key"; // 登录的用户头像
-    public final static String USER_HEADER_TYPE_KEY = "user_header_type_key"; // 登录的头像类型
-
-    public final static String KEY_USER_HEADER_NFT_URL = "key_user_header_nft_url"; // 登录的头像nft图片
 
     public final static String KEY_LANG_CELL_TYPE = "key_lang_cell_type"; // 记录当前设置的语言类型
     public final static String KEY_SHOW_GUIDE_UPGRADE_TIMESTAMP = "key_show_guide_upgrade_timestamp"; // 记录上一次显示引导更新的时间戳
 
-    public static final String KEY_NFT_BIND_WALLET = "key_nft_bind_wallet"; // 已绑定的钱包信息
-
-    public final static String KEY_SHOWN_CHANGE_ADDRESS_GUIDE = "key_shown_change_address_guide"; // 是否已经展示过切换地址的引导
-    public final static String KEY_SHOWN_CHANGE_NETWORK_GUIDE = "key_shown_change_network_guide"; // 是否已经展示过切换网络的引导
+    public final static String KEY_USER_HEADER_NFT_URL = "key_user_header_nft_url_v1"; // 登录的头像nft图片
+    public final static String USER_HEADER_TYPE_KEY = "user_header_type_key_v1"; // 登录的头像类型
+    public static final String KEY_NFT_BIND_WALLET = "key_nft_bind_wallet_v1"; // 已绑定的钱包信息
+    public final static String KEY_SHOWN_CHANGE_ADDRESS_GUIDE = "key_shown_change_address_guide_v1"; // 是否已经展示过切换地址的引导
+    public final static String KEY_SHOWN_CHANGE_NETWORK_GUIDE = "key_shown_change_network_guide_v1"; // 是否已经展示过切换网络的引导
 
     public static SPUtils getSP() {
         return SPUtils.getInstance(SP_NAME);

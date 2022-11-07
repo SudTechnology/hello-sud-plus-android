1420f64bd6e3eef77803b5bcbfc47694ddd0acd1
liwensi <liwensi@sud.tech>
2022-11-07 18:26:16 +0800

    feat：使用模拟器路径加载火箭

--- project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/base/activity/BaseRocketRoomActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/base/activity/BaseRocketRoomActivity.java
@@ -61,8 +61,8 @@ public abstract class BaseRocketRoomActivity<T extends AppGameViewModel> extends
         // TODO: 2022/11/7 下面的代码需要去掉的
         File dir = Environment.getExternalStorageDirectory();
         File file = new File(dir.getAbsolutePath() + File.separator + "Pictures", "rocket.rpk");
-//        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, file.getAbsolutePath());
-        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, "rocket.rpk");
+        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, file.getAbsolutePath());
+//        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, "rocket.rpk");
 
         rocketGameViewModel.fragmentActivity = this;
         rocketGameViewModel.roomId = roomInfoModel.roomId;

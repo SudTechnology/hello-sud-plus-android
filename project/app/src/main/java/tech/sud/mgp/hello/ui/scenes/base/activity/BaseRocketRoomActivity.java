ede4a778a953b68757bd385585142a1308733918
liwensi <liwensi@sud.tech>
2022-11-08 11:36:17 +0800

    feat(rocket)：使用后台配置的火箭

--- project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/base/activity/BaseRocketRoomActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/base/activity/BaseRocketRoomActivity.java
@@ -1,13 +1,10 @@
 package tech.sud.mgp.hello.ui.scenes.base.activity;
 
-import android.Manifest;
-import android.os.Environment;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import androidx.lifecycle.Observer;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -16,19 +13,15 @@ import tech.sud.mgp.SudMGPWrapper.state.SudMGPAPPState;
 import tech.sud.mgp.SudMGPWrapper.state.SudMGPAPPState.AppCustomRocketPlayModelList;
 import tech.sud.mgp.SudMGPWrapper.state.SudMGPMGState;
 import tech.sud.mgp.SudMGPWrapper.utils.SudJsonUtils;
-import tech.sud.mgp.core.SudMGP;
 import tech.sud.mgp.hello.R;
 import tech.sud.mgp.hello.common.base.BaseDialogFragment;
 import tech.sud.mgp.hello.common.http.rx.RxCallback;
 import tech.sud.mgp.hello.common.model.HSUserInfo;
-import tech.sud.mgp.hello.common.utils.permission.PermissionFragment;
-import tech.sud.mgp.hello.common.utils.permission.SudPermissionUtils;
 import tech.sud.mgp.hello.common.widget.dialog.SimpleChooseDialog;
 import tech.sud.mgp.hello.service.game.repository.GameRepository;
 import tech.sud.mgp.hello.service.game.req.RocketFireReq;
 import tech.sud.mgp.hello.service.game.resp.RocketFireResp;
 import tech.sud.mgp.hello.ui.common.dialog.LoadingDialog;
-import tech.sud.mgp.hello.ui.main.base.constant.GameIdCons;
 import tech.sud.mgp.hello.ui.scenes.base.model.AudioRoomMicModel;
 import tech.sud.mgp.hello.ui.scenes.base.model.UserInfo;
 import tech.sud.mgp.hello.ui.scenes.base.utils.UserInfoConverter;
@@ -59,9 +52,9 @@ public abstract class BaseRocketRoomActivity<T extends AppGameViewModel> extends
         rocketContainer = findViewById(R.id.rocket_container);
 
         // TODO: 2022/11/7 下面的代码需要去掉的
-        File dir = Environment.getExternalStorageDirectory();
-        File file = new File(dir.getAbsolutePath() + File.separator + "Pictures", "rocket.rpk");
-        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, file.getAbsolutePath());
+//        File dir = Environment.getExternalStorageDirectory();
+//        File file = new File(dir.getAbsolutePath() + File.separator + "Pictures", "rocket.rpk");
+//        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, file.getAbsolutePath());
 //        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, "rocket.rpk");
 
         rocketGameViewModel.fragmentActivity = this;
@@ -284,15 +277,16 @@ public abstract class BaseRocketRoomActivity<T extends AppGameViewModel> extends
     @Override
     protected void onResume() {
         super.onResume();
-        SudPermissionUtils.requirePermission(this, getSupportFragmentManager(),
-                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                new PermissionFragment.OnPermissionListener() {
-                    @Override
-                    public void onPermission(boolean success) {
-                        if (success) {
-                        }
-                    }
-                });
+        // TODO: 2022/11/8 正式发版要去掉
+//        SudPermissionUtils.requirePermission(this, getSupportFragmentManager(),
+//                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+//                new PermissionFragment.OnPermissionListener() {
+//                    @Override
+//                    public void onPermission(boolean success) {
+//                        if (success) {
+//                        }
+//                    }
+//                });
     }
 
     /** 打开火箭 */

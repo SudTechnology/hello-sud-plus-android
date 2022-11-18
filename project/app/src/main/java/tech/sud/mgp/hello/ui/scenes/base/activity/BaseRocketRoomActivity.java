16648b8cc2c0e98372de55c51cab36fb36e2135c
liwensi <liwensi@sud.tech>
2022-11-18 18:36:34 +0800

    feat(rocket)：使用模拟器火箭包路径，用于调试

--- project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/base/activity/BaseRocketRoomActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/scenes/base/activity/BaseRocketRoomActivity.java
@@ -1,5 +1,7 @@
 package tech.sud.mgp.hello.ui.scenes.base.activity;
 
+import android.Manifest;
+import android.os.Environment;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -8,6 +10,7 @@ import androidx.lifecycle.Observer;
 
 import com.jeremyliao.liveeventbus.LiveEventBus;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -16,16 +19,20 @@ import tech.sud.mgp.SudMGPWrapper.state.SudMGPAPPState;
 import tech.sud.mgp.SudMGPWrapper.state.SudMGPAPPState.AppCustomRocketPlayModelList;
 import tech.sud.mgp.SudMGPWrapper.state.SudMGPMGState;
 import tech.sud.mgp.SudMGPWrapper.utils.SudJsonUtils;
+import tech.sud.mgp.core.SudMGP;
 import tech.sud.mgp.hello.R;
 import tech.sud.mgp.hello.common.base.BaseDialogFragment;
 import tech.sud.mgp.hello.common.event.LiveEventBusKey;
 import tech.sud.mgp.hello.common.event.model.JumpRocketEvent;
 import tech.sud.mgp.hello.common.http.rx.RxCallback;
+import tech.sud.mgp.hello.common.utils.permission.PermissionFragment;
+import tech.sud.mgp.hello.common.utils.permission.SudPermissionUtils;
 import tech.sud.mgp.hello.common.widget.dialog.SimpleChooseDialog;
 import tech.sud.mgp.hello.service.game.repository.GameRepository;
 import tech.sud.mgp.hello.service.game.req.RocketFireReq;
 import tech.sud.mgp.hello.service.game.resp.RocketFireResp;
 import tech.sud.mgp.hello.ui.common.dialog.LoadingDialog;
+import tech.sud.mgp.hello.ui.main.base.constant.GameIdCons;
 import tech.sud.mgp.hello.ui.scenes.base.model.AudioRoomMicModel;
 import tech.sud.mgp.hello.ui.scenes.base.model.UserInfo;
 import tech.sud.mgp.hello.ui.scenes.base.viewmodel.AppGameViewModel;
@@ -61,18 +68,18 @@ public abstract class BaseRocketRoomActivity<T extends AppGameViewModel> extends
         tvCloseRocketEffect = findViewById(R.id.tv_close_rocket_effect);
 
         // TODO: 2022/11/7 下面的代码需要去掉的
-//        SudPermissionUtils.requirePermission(this, getSupportFragmentManager(),
-//                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-//                new PermissionFragment.OnPermissionListener() {
-//                    @Override
-//                    public void onPermission(boolean success) {
-//                        if (success) {
-//                        }
-//                    }
-//                });
-//        File dir = Environment.getExternalStorageDirectory();
-//        File file = new File(dir.getAbsolutePath() + File.separator + "Pictures", "rocket.rpk");
-//        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, file.getAbsolutePath());
+        SudPermissionUtils.requirePermission(this, getSupportFragmentManager(),
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                new PermissionFragment.OnPermissionListener() {
+                    @Override
+                    public void onPermission(boolean success) {
+                        if (success) {
+                        }
+                    }
+                });
+        File dir = Environment.getExternalStorageDirectory();
+        File file = new File(dir.getAbsolutePath() + File.separator + "Pictures", "rocket.rpk");
+        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, file.getAbsolutePath());
 
 //        SudMGP.getCfg().addEmbeddedMGPkg(GameIdCons.CUSTOM_ROCKET, "rocket.rpk");
 

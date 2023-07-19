621fb89639f1b3dc0843dbd9f89726e1071848ff
朱星星 <zhuxingxing@divtoss.com>
2023-07-19 17:34:15 +0800

    develop: 更新SudMGPWrapper

--- project/SudMGPWrapper/src/main/java/tech/sud/mgp/SudMGPWrapper/state/SudMGPMGState.java
+++ project/SudMGPWrapper/src/main/java/tech/sud/mgp/SudMGPWrapper/state/SudMGPMGState.java
@@ -383,20 +383,6 @@ public class SudMGPMGState implements Serializable {
     public static class MGCommonGameASR implements Serializable {
         // true:打开语音识别 false:关闭语音识别
         public boolean isOpen;
-        // 必填字段；关键词列表，可以传送多个关键词
-        public List<String> wordList;
-        // 关键词，每一轮都会下发，不区分角色
-        public String realWord;
-        // 必填字段；关键词语言，默认:zh-CN(老版本游戏可能没有)；透传
-        public String wordLanguage;
-        // 必填字段；text:文本包含匹配; number:数字等于匹配(必填字段)；默认:text(老版本游戏可能没有)；数字炸弹填number；透传
-        public String wordType;
-        // 必填字段；false: 命中不停止；true:命中停止(必填字段)；默认:true(老版本游戏可能没有) 你演我猜填false；透传
-        public boolean isCloseConnHitted;
-        // 必填字段，是否需要匹配关键字， 默认是true,   如果是false, 则只简单的返回语音识别文本；透传
-        public boolean enableIsHit;
-        // 必填字段，是否需要返回转写文本，默认是true
-        public boolean enableIsReturnText;
     }
 
     /**

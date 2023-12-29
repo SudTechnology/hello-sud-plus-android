171640f17a990db882a5c10820827779c36140ef
liwensi <liwensi@sud.tech>
2023-12-29 11:12:35 +0800

    feat：首页tab名称改为场景

--- project/app/src/main/java/tech/sud/mgp/hello/ui/main/base/activity/MainActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/main/base/activity/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends BaseActivity implements MainTabView.TabClickLi
      * 初始化Tab数据
      */
     private void initTabs() {
-        tabs.add(new TabModel(0, getString(R.string.tabs_index), R.drawable.icon_home_index));
+        tabs.add(new TabModel(0, getString(R.string.scene), R.drawable.icon_home_index));
         tabs.add(new TabModel(1, getString(R.string.game), R.drawable.ic_game_list));
         tabs.add(new TabModel(2, getString(R.string.tabs_room), R.drawable.icon_home_room));
         tabs.add(new TabModel(3, getString(R.string.mine), R.drawable.icon_mine_tabbar_select_24));

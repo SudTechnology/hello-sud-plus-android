f135e2ff4af04add0180d5652daa0810969109a1
liwensi <liwensi@sud.tech>
2024-04-03 14:50:44 +0800

    feat：游戏Tab和场景Tab互换位置

--- project/app/src/main/java/tech/sud/mgp/hello/ui/main/base/activity/MainActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/main/base/activity/MainActivity.java
@@ -139,8 +139,8 @@ public class MainActivity extends BaseActivity implements MainTabView.TabClickLi
      * 初始化Tab数据
      */
     private void initTabs() {
-        tabs.add(new TabModel(0, getString(R.string.scene), R.drawable.icon_home_index));
-        tabs.add(new TabModel(1, getString(R.string.game), R.drawable.ic_game_list));
+        tabs.add(new TabModel(0, getString(R.string.game), R.drawable.ic_game_list));
+        tabs.add(new TabModel(1, getString(R.string.scene), R.drawable.icon_home_index));
         tabs.add(new TabModel(2, getString(R.string.tabs_room), R.drawable.icon_home_room));
         tabs.add(new TabModel(3, getString(R.string.mine), R.drawable.icon_mine_tabbar_select_24));
         for (int i = 0; i < tabs.size(); i++) {
@@ -199,10 +199,10 @@ public class MainActivity extends BaseActivity implements MainTabView.TabClickLi
         public Fragment createFragment(int position) {
             switch (position) {
                 case 0: {
-                    return HomeFragment.newInstance();
+                    return GameListFragment.newInstance();
                 }
                 case 1: {
-                    return GameListFragment.newInstance();
+                    return HomeFragment.newInstance();
                 }
                 case 2: {
                     return RoomFragment.newInstance();

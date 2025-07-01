50621fbe4b2af51b18a3d5ace0ec0ecb38af3f52
liwensi <liwensi@sud.tech>
2025-07-02 00:33:01 +0800

    feat：修改大模型AI tab的名称为大模型

--- project/app/src/main/java/tech/sud/mgp/hello/ui/main/base/activity/MainActivity.java
+++ project/app/src/main/java/tech/sud/mgp/hello/ui/main/base/activity/MainActivity.java
@@ -153,7 +153,7 @@ public class MainActivity extends BaseActivity implements MainTabView.TabClickLi
     private void initTabs() {
         tabs.add(new TabModel(0, getString(R.string.game), R.drawable.ic_game_list));
         tabs.add(new TabModel(1, getString(R.string.scene), R.drawable.icon_home_index));
-        tabs.add(new TabModel(2, getString(R.string.large_model_ai), R.drawable.ic_llm, R.drawable.ic_llm_selected, true));
+        tabs.add(new TabModel(2, getString(R.string.large_model), R.drawable.ic_llm, R.drawable.ic_llm_selected, true));
         tabs.add(new TabModel(3, getString(R.string.tabs_room), R.drawable.icon_home_room));
         tabs.add(new TabModel(4, getString(R.string.mine), R.drawable.icon_mine_tabbar_select_24));
         for (int i = 0; i < tabs.size(); i++) {
